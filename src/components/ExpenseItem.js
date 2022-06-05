85d94b9b10917e710ae649ab3307fd254cb1c4ee
Erudite885 <erudite885@gmail.com>
2022-06-05 10:36:40 +0100

    ExpenseItem.js receives props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
+// const expenseDate = new Date(2022, 6, 6);
+// const expenseTitle = 'Car Insurance';
+// const expenseAmount = 255.99;
+
   return (
     <div className='expense-item'>
-        <div>Date: June 5th 2022</div>
+        <div> Date: {props.date.toISOString()} </div>
         <div className='expense-item__description'>
-            <h2>Title: Car Insurance</h2>
-            <div className='expense-item__price'>Amount: $255.99</div>
+            <h2> Title: {props.title} </h2>
+            <div className='expense-item__price'> $ {props.amount} </div>
         </div>
     </div>
   )
