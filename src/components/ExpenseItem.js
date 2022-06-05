ff459b834d08ed247581723815eaf624f4c3f141
Erudite885 <erudite885@gmail.com>
2022-06-05 10:51:54 +0100

    dynamic date property added to expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -8,9 +8,18 @@ const ExpenseItem = (props) => {
 // const expenseTitle = 'Car Insurance';
 // const expenseAmount = 255.99;
 
+const month = props.date.toLocaleString('en-US', {month: 'long'}); 
+const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+const year = props.date.getFullYear();
+
   return (
     <div className='expense-item'>
-        <div> Date: {props.date.toISOString()} </div>
+        <div>
+            <div>Month: {month}</div>
+            <div>Year: {year}</div>
+            <div>Day: {day} </div>
+        </div>
+        {/* <div> Date: {props.date.toISOString()} </div> */}
         <div className='expense-item__description'>
             <h2> Title: {props.title} </h2>
             <div className='expense-item__price'> $ {props.amount} </div>
