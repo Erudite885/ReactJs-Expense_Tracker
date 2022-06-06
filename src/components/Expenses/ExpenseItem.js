def9ce13de3b7780f5d62076646d160f7b4bc2f6
Erudite885 <erudite885@gmail.com>
2022-06-06 03:23:28 +0100

    multiple state logic updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-
 // const expenseDate = new Date(2022, 6, 6);
 // const expenseTitle = 'Car Insurance';
 // const expenseAmount = 255.99;
-    
 
+ const [title, setTitle] = useState(props.title);
 
   return (
     <Card className='expense-item'>
@@ -20,6 +19,7 @@ const ExpenseItem = (props) => {
             <h2> {props.title} </h2>
             <div className='expense-item__price'> $ {props.amount} </div>
         </div>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   )
 }
