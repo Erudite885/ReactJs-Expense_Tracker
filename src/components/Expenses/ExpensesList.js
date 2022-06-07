e4a3b80f226a753ec19c7aa04210c150ceb293d2
Erudite885 <erudite885@gmail.com>
2022-06-07 08:45:44 +0100

    expensesContent put to use

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -8,7 +8,7 @@ const ExpensesList = (props) => {
     let expensesContent =  <p>No Expenses Found. </p>;
 
     if (props.items.length === 0) {
-       return <h2 className='expenses-list__fallback'> Found no expenses. </h2>;
+       return <h2 className='expenses-list__fallback'> {/*Found no expenses.*/} {expensesContent} </h2>;
     }
 
     return <ul className='expenses-list'>
