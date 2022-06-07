0cfb55ee168af15e97fd944a1de0d657d8842def
Erudite885 <erudite885@gmail.com>
2022-06-07 13:36:02 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// import ReactDOM from 'react-dom/client';
+
+// import './index.css';
+// import App from './App';
+
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container)
+// root.Render(<App />);
